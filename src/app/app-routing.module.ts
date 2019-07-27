d1a1a6a5d442f944ab4b0a5acb1b06784585371d
Pedro <pedro.silva-dev@hotmail.com>
2019-07-27 01:00:06 -0300

    Proteção de rotas | Verifica se usuario está logado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule', canActivate: [AuthGuard] },
+  //essa rota só sera ativada se o guard especificado for true que é um usuario logado
+  //Assim vai sempre verificar se tem usuario ou não através do metodo do firebase chamado pelo getAuth()
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule', canActivate: [LoginGuard]},
 ];
 
 @NgModule({
--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { CanActivate, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginGuard implements  {
+export class LoginGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+  canActivate(): Promise<boolean> {
+    return new Promise(resolve => {
+      this.authService.getAuth().onAuthStateChanged(user => {
+        if (user) this.router.navigate(['home']) //verifica se existe usuario, se sim manda para home
+
+        resolve(!user ? true : false) 
+      })
+    })
+  }
   
 }
