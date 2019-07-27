e0f2138b8b6110939a7d114178d887775262fbba
Pedro <pedro.silva-dev@hotmail.com>
2019-07-26 23:46:31 -0300

    Como tratar erros de cadastro do firebase

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -9,13 +9,13 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  @ViewChild(IonSlides) slides : IonSlides; //1
+  @ViewChild(IonSlides) slides: IonSlides; //1
   public usuarioLogin: Usuario = {};
   public usuarioCadastro: Usuario = {};
 
   public wavesPosition: number = 0;   //fazer o movimento das ondas EXEMPLO 2
   public wavesDiference: number = 80; //+= para incrementar uma variavel com outra
-                                      //só tem a fuidez por causa do transition na classe no CSS.
+  //só tem a fuidez por causa do transition na classe no CSS.
 
   private loading: any;
 
@@ -30,16 +30,16 @@ export class LoginPage implements OnInit {
   }
 
   segmentChanged(event) {
-    console.log(event);  
+    console.log(event);
     // O evento retorna um CustomEvent e uma das propriedades (detail) contem o valor do evento.
     //Para manipular elementos do HTML pelo TS usa-se o @ViewChild passando o componente ou 
     //identificador(#slider) EXEMPLO 1.
 
-    if(event.detail.value === 'login') {
-      this.slides.slidePrev(); 
+    if (event.detail.value === 'login') {
+      this.slides.slidePrev();
       this.wavesPosition += this.wavesDiference; //2
     } else {
-      this.slides.slideNext(); 
+      this.slides.slideNext();
       this.wavesPosition -= this.wavesDiference; //2
 
     }
@@ -48,24 +48,35 @@ export class LoginPage implements OnInit {
   login() {
 
   }
- 
+
   async cadastro() {
     await this.presentLoading();
 
     try {
       await this.authService.cadastro(this.usuarioCadastro)
-    } catch(erro) {
+    } catch (erro) {
       console.error(erro);
-      this.presentToast(erro.message)
+      let message: string
+
+      switch (erro.code) {
+        case 'auth/email-already-in-use':
+          message = "Email já cadastrado";
+          break;
+
+        case 'auth/invalid-email':
+          message = "Formato de email invalido";
+          break;
+      }
+      this.presentToast(message)
     } finally {
       this.loading.dismiss();
     }
-     
+
     console.log(this.usuarioCadastro);
   }
 
   async presentLoading() {
-     this.loading = await this.loadingCtrl.create({
+    this.loading = await this.loadingCtrl.create({
       message: 'Aguarde',
     });
     return this.loading.present();
@@ -73,9 +84,9 @@ export class LoginPage implements OnInit {
 
   async presentToast(message: string) {
     const toast = await this.toastCtrl.create({
-      message, 
+      message,
       //No EQ se o nome do parametro recebido for o mesmo da var pode usar assim ao inves de message: message.
-      duration: 2000
+      duration: 5000
     });
     toast.present();
   }
