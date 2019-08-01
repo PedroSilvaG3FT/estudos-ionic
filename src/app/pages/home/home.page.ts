95e420a2cdc0e9a2685be6287bd4979fbaee05e5
Pedro <pedro.silva-dev@hotmail.com>
2019-07-31 23:05:09 -0300

    Adicionando a chamada da lista na tela de lista de produtos

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { Produto } from 'src/app/models/produto';
+import { Subscription } from 'rxjs';
+import { ProdutoService } from 'src/app/services/produto.service';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +10,18 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  public products = new Array<Produto>();
+  private productsSubscription: Subscription;
 
   constructor(
     private authService: AuthService,
-  ) { }
+    private produtoService: ProdutoService
+  ) { 
+    //Chamando lista de produtos
+    this.productsSubscription = this.produtoService.getAll().subscribe(data => {
+      this.products = data;
+    });
+  }
 
   ngOnInit() {
   }
