fb96da01c5e1dbc2156b4f2f060bf325e2383005
Pedro <pedro.silva-dev@hotmail.com>
2019-07-31 22:40:01 -0300

    Adicionando referencia a tabela de produtos do firebase

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { Produto } from '../models/produto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
+  private produtoCollection: AngularFirestoreCollection<Produto>;
+  //referencia a collection do firebase e especificando o tipo que é a model
 
-  constructor() { }
+  constructor(private angularFireAuth: AngularFirestore) {
+    this.produtoCollection = this.angularFireAuth.collection<Produto>('Produto');
+    //Colocando nome na collection/tabela do firebase
+  }
 }
